c92c909e0b93fe7ad74371d309d08ba0f0dd400d
Arhaam Patvi <thearhaam@gmail.com>
2020-03-21 12:17:24 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# PROJECT: DETECTION AND ANALYSIS OF NEGATIVE EMOTIONS ON SOCIAL MEDIA USING MACHINE LEARNING
+# Name:         Arhaam Patvi
+# PRN:          17070124501
+# Institute:    Symbiosis Institute of Technology
+# Branch:       IT
+# Batch:        2016-20
+
